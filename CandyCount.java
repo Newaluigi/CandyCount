276e5083e24709ea917cea630a22e27c3f000e83
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-06-28 16:15:31 +0200

    first commit

---
+++ CandyCount.java
@@ -0,0 +1,16 @@
+public class CandyCount {
+    public static void main(String[] args) {
+        double money = 12.4;
+        double price = 1.2;
+        int candies = 0;
+        
+        if (money > 0 && price > 0) {
+            while (money - price >= 0) {
+                candies++;
+                money -= price;
+            }
+        }
+        
+        System.out.println(candies);
+    }
+}
